8a6bcd6fb04d2103a4de84f1642991daae0113c5
laraluitingh <61213237+laraluitingh@users.noreply.github.com>
2021-09-16 18:35:01 +1000

    changes

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 import psycopg2
+import os
+
+DB_URL = os.environ.get("DATABASE_URL", "dbname=chase-stocks")
 
 def sql_select(query,params):
-  conn = psycopg2.connect("dbname=chase-stocks")
+  conn = psycopg2.connect(DB_URL)
   cur = conn.cursor()
   cur.execute(query, params)
   results = cur.fetchall()
@@ -10,14 +13,14 @@ def sql_select(query,params):
   return results
 
 def sql_write(query, params):
-  conn = psycopg2.connect("dbname=chase-stocks")
+  conn = psycopg2.connect(DB_URL)
   cur = conn.cursor()
   cur.execute(query, params)
   conn.commit()
   conn.close()
 
 def sql_select_without_params(query):
-  conn = psycopg2.connect("dbname=chase-stocks")
+  conn = psycopg2.connect(DB_URL)
   cur = conn.cursor()
   cur.execute(query)
   results = cur.fetchall()
